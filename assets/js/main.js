aa541d0222e2516e00b9c8b3adf2c45e75ed83e5
Avinash Kumar Saw <avinashw50w@gmail.com>
2016-12-29 12:12:30 +0530

    changed jumbotron cover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
 
     setTimeout(function() {
-        $('.jumbotron').css("background", "url('assets/img/back3.jpg')");
 
         $(document).on('scroll', function (e) {
             $('.navbar').css('opacity', ($(document).scrollTop() / 400));
         });
 
         $(".se-pre-con").fadeOut("slow");
-        
+
     },2000);
 
 });
