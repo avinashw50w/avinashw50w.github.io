0704b6f51e5ad049b8d94d1a0cadd41a6613ccf6
Avinash Kumar Saw <avinashw50w@gmail.com>
2016-12-29 09:49:16 +0530

    added loading icon feature which plays for 2 secs until page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
-$(document).ready(
-    $(document).on('scroll', function (e) {
-        $('.navbar').css('opacity', ($(document).scrollTop() / 400));
-    })
-);
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('.jumbotron').css("background", "url('assets/img/back3.jpg')");
+
+        $(document).on('scroll', function (e) {
+            $('.navbar').css('opacity', ($(document).scrollTop() / 400));
+        });
+
+        $(".se-pre-con").fadeOut("slow");
+        
+    },2000);
+
+});
