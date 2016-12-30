3f53c285c49a620ce3a03096539422453feb9d63
Avinash Kumar Saw <avinashw50w@gmail.com>
2016-12-30 15:35:27 +0530

    added scrolling feautes to navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,24 @@ $(document).ready(function() {
 
     setTimeout(function() {
 
+        $("#top").on('click', function(e) {
+            $('html,body').animate({scrollTop:0},'slow');
+        });
+
+        $("nav ul li a[href^='#']").on('click', function(e) {
+
+            var target = this.hash;
+
+            e.preventDefault();
+            var navoffset = $("#my-navbar").height();
+
+            $('html, body').animate({
+                scrollTop: $(target).offset().top - navoffset
+            }, 'slow', function() {
+                return window.history.pushState(null, null, target);
+            });
+        });
+
         $(document).on('scroll', function (e) {
             $('.navbar').css('opacity', ($(document).scrollTop() / 400));
         });
