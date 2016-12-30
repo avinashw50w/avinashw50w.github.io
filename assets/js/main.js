6a06f1c498c877314a8575837f2f505e064a19bf
Avinash Kumar Saw <avinashw50w@gmail.com>
2016-12-30 23:24:48 +0530

    removed setTimeout() for preloading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
 
-    setTimeout(function() {
+    $(".se-pre-con").fadeOut("slow");
 
-        $("#top").on('click', function(e) {
-            $('html,body').animate({scrollTop:0},'slow');
+    $("#top").on('click', function(e) {
+        $('html,body').animate({scrollTop:0},'slow');
+    });
+
+    $("nav ul li a[href^='#']").on('click', function(e) {
+
+        var target = this.hash;
+
+        e.preventDefault();
+        var navoffset = $("#my-navbar").height();
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - navoffset
+        }, 'slow', function() {
+            return window.history.pushState(null, null, target);
         });
+    });
 
-        $("nav ul li a[href^='#']").on('click', function(e) {
 
-            var target = this.hash;
+    $(document).on('scroll', function (e) {
+        var windowOffset = $(window).height();
+        var navOffset = $("#my-navbar").height();
 
-            e.preventDefault();
-            var navoffset = $("#my-navbar").height();
-
-            $('html, body').animate({
-                scrollTop: $(target).offset().top - navoffset
-            }, 'slow', function() {
-                return window.history.pushState(null, null, target);
-            });
-        });
-
-        $(document).on('scroll', function (e) {
-            $('.navbar').css('opacity', ($(document).scrollTop() / 400));
-        });
-
-        $(".se-pre-con").fadeOut("slow");
-
-    },2000);
+        $('.navbar').css('opacity', ($(document).scrollTop() / (windowOffset - navOffset)));
+    });
 
 });
